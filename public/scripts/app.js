7d93a03ad4d73c49f601d8f59d1afba12edb18b7
Huan Ming Liao <hmliao14@gmail.com>
2017-09-05 22:36:47 -0700

    added initial profile form css fonts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,8 +9,19 @@ $(document).ready(function() {
 	for(var i = 1; i <= board_size; i ++){
 		$board.append(templateRow(i));
 	}
+	console.log("clicked");
+	$('.edit-btn').on("click", displayForm);
+
 
 });
+
+function displayForm(event){
+	$('.toggle').toggle();
+}
+
+function updateProfile(event) {
+
+}
 
 // Create the game board
 function templateRow(i){
