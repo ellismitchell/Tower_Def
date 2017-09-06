ebb0bb95c3601e9b5cc824a6aed2fb7fd3fb1681
Huan Ming Liao <hmliao14@gmail.com>
2017-09-06 11:13:25 -0700

    Added initial minion, tower, and bullet movements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,8 +20,69 @@ $(document).ready(function() {
 	console.log("clicked");
 	$('.edit-btn').on("click", displayForm);
 
+	// Draw the guy and move across
+	$('#31').append(`<img src="/imgs/car1.jpg" class="minion" id="m1">`)
+
+	// cW : column width
+	// .offset() calculates from the center
+	var minionIntervalID = setInterval(minion_move, 100);
+	var cW = 150;
+
+
+	function minion_move() {
+		var minion = $('#m1');
+		var column = $('#36');
+		// checking the minion x > column's x+width 
+		if(minion.offset().left >= column.offset().left+cW/2){
+			console.log(minion.offset().left);
+			console.log(column.offset().left);
+			window.clearInterval(minionIntervalID);
+		}
+		minion.animate({
+			"margin-left": "+=20px"
+		}, 100);
+	}
+
+	// Place tower
+	var $tower = $('#43');
+	$tower.append(`<img src="/imgs/tower.jpg" class="tower" id="t1">`);
+
+	// Create bullet incrementally
+	var $tower = $('#43');
+	$tower.append(`<img src="/imgs/car1.jpg" class="bullet" id="b1">`);
+	
+	shoot_bullet();
+
+	function shoot_bullet(){
+		var minion = $('#m1');
+		var bullet = $('#b1');
+		var minionX  = minion.offset().left;
+		var minionY = minion.offset().top;
+		var bulletX = bullet.offset().left;
+		var bulletY = bullet.offset().top;
+		var xDistance = minionX - bulletX;
+		var yDistance = minionY - bulletY;
+		bullet.animate({
+			"margin-left": `+=${xDistance}`,
+			"margin-top": `+=${yDistance}`,
+		}, 1000, "swing", function removeBullet(){
+			console.log("removed");
+			bullet.remove();
+		});
+	}
+
+	// function removeBullet(bullet) {
+
+	// }
+
+
+
 
 });
+
+
+
+
 
 function displayForm(event){
 	$('.toggle').toggle();
@@ -34,7 +95,7 @@ function updateProfile(event) {
 // Create the game board
 function templateRow(x,y){
 	return`
-		<div class="col col-2" id="${x}${y}">
+		<div class="column" id="${x}${y}">
 		</div>
 	`;
 }
