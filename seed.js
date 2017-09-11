2b991783e702402cda7d0e417cb8e85fcf35e758
Huan Ming Liao <hmliao14@gmail.com>
2017-09-11 11:24:42 -0700

    added more artwork and data to seed

--- seed.js
+++ seed.js
@@ -3,45 +3,76 @@ let waveList = [];
 waveList.push({
 	wave: 1,
 	minions: [
-		{image: "/imgs/fighter.png", speed: 30, hp: 100, pathIndex: 0, alive: true, gold: 25},
-		{image: "/imgs/fighter.png", speed: 30, hp: 100, pathIndex: 0, alive: true, gold: 25},
-		{image: "/imgs/fighter.png", speed: 30, hp: 100, pathIndex: 0, alive: true, gold: 25},
-		{image: "/imgs/fighter.png", speed: 30, hp: 100, pathIndex: 0, alive: true, gold: 25},
-		{image: "/imgs/fighter.png", speed: 30, hp: 100, pathIndex: 0, alive: true, gold: 25}
+		{image: "/imgs/zombie.png", speed: 10, hp: 220, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/zombie.png", speed: 10, hp: 220, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/zombie.png", speed: 10, hp: 220, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/zombie.png", speed: 10, hp: 220, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/zombie.png", speed: 10, hp: 220, pathIndex: 0, alive: true, gold: 30}
 	]
 });
 waveList.push({
 	wave: 2,
 	minions: [
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
-		{image: "/imgs/car1.png", speed: 40, hp: 150, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
+		{image: "/imgs/car1.png", speed: 30, hp: 150, pathIndex: 0, alive: true, gold: 30},
 	]
 });
 waveList.push({
 	wave: 3,
 	minions: [
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
-		{image: "/imgs/green_fighter.png", speed: 50, hp: 250, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+		{image: "/imgs/green_fighter.png", speed: 40, hp: 175, pathIndex: 0, alive: true, gold: 35},
+	]
+});
+waveList.push({
+	wave: 4,
+	minions: [
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+		{image: "/imgs/spaceship.png", speed: 35, hp: 200, pathIndex: 0, alive: true, gold: 40},
+	]
+});
+waveList.push({
+	wave: 5,
+	minions: [
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
+		{image: "/imgs/green_car.png", speed: 35, hp: 300, pathIndex: 0, alive: true, gold: 45},
 	]
 });
 db.Wave.remove({}, function(err, waves){
 	db.Wave.create(waveList, function(err, waves){
+		console.log("seed minion succeed!");
 		process.exit();
 	})
 });
